d0abae7cc66a33b7320c8af6a3fd4989e0b38c90
Kalil Salkey <ksalkey@gmail.com>
2019-01-09 19:04:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
  if board[index] == "X" || board[index] == "O"&& index.between?(0, 8)
-    return false 
+  
   elsif 
     board[index] == " "||board[index] == ""|| board[index] == nil && index.between?(0, 8)
     return true 
